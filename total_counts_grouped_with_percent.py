afe9c85d1d0ae69f51a0be138917c57a9958ddc0
Pat Escalona <pat.escalona7@gmail.com>
2023-07-10 23:06:41 -0500

    script works including saving to excel

--- total_counts_grouped_with_percent.py
+++ total_counts_grouped_with_percent.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-
 import pandas as pd
 import pyreadstat
+from openpyxl import load_workbook
 
 # Read SPSS .sav file
 data, meta = pyreadstat.read_sav(
@@ -12,6 +12,9 @@ df_copy = pyreadstat.set_value_labels(data, meta)
 
 # List of variable names
 values_list = ["TTQ3.1_1", "TTQ3.1_2", "TTQ3.1_3"]
+
+# excel file to save result to
+excel_file = "../taste_test_scripts/grouped_frequencies.xlsx"
 
 # Select variables from the SPSS dataset
 selected_variables = [
@@ -25,18 +28,29 @@ combined_df = pd.concat(
     ignore_index=True,
 ).dropna(axis=0)
 
-# count responses by variable value
+# count responses by variable value and get total counts for combined.df
 value_counts = combined_df.value_counts(normalize=True).sort_index()
 total_counts = combined_df.value_counts().sum()
 
-# get total number of counts
-# total_counts = value_counts.sum()
-# print(value_counts)
-# print(f"Total counts: {total_counts}")
 print(f"{value_counts}\nTotal counts: {total_counts}")
-# print(f"Total counts: {total_counts}")
 
-# todo
 # Save to Excel
-# result = pd.concat([value_counts, total_counts])
-# print(result)
+result = pd.concat(
+    [value_counts, pd.Series(total_counts, index=["Total counts"])], axis=1
+)
+result.columns = ["Value counts", "Total counts"]
+
+# Open the existing workbook
+book = load_workbook(excel_file)
+
+# Create a new worksheet with the name of the first value in values_list
+worksheet_name = values_list[0]
+
+# Append the DataFrame to the existing workbook
+with pd.ExcelWriter(
+    "../taste_test_scripts/grouped_frequencies.xlsx",
+    if_sheet_exists="overlay",
+    engine="openpyxl",
+    mode="a",
+) as writer:
+    result.to_excel(writer, sheet_name=worksheet_name, index_label="Variable")
